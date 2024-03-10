630759d1d6d2f6c5d77f4e88715462c43fe9bc5a
adamascencio <ascencioadam@gmail.com>
2024-03-10 14:27:28 -0700

    Create contact list component to render contacts retrieved from db

---
+++ frontend/src/ContactList.jsx
@@ -0,0 +1,39 @@
+import { ContactType } from "./types/ContactType";
+import PropTypes from 'prop-types';
+
+const ContactList = ({ contacts }) => {
+  return (
+    <div>
+      <h2>Contacts</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map(contact => (
+            <tr key={contact.id}>
+              <td>{contact.firstName}</td>
+              <td>{contact.lastName}</td>
+              <td>{contact.email}</td>
+              <td>
+                <button>Edit</button>
+                <button>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(ContactType).isRequired
+}
+
+export default ContactList;
