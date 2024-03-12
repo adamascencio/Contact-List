346d95d9b0d605dc22461d25076e7c34a1542fca
adamascencio <ascencioadam@gmail.com>
2024-03-12 09:54:54 -0700

    Add additional http request method for updating contacts

--- frontend/src/ContactForm.jsx
+++ frontend/src/ContactForm.jsx
@@ -13,7 +13,7 @@ const ContactForm = ({ existingContact = {}, updateCallback }) => {
     const data = { firstName, lastName, email };
     const url = "http://127.0.0.1:5000/" + (updating ? `update_contact/${existingContact.id}` : "create_contact")
     const options = {
-      method: "POST",
+      method: updating ? "PUT" : "POST",
       headers: {
         "Content-Type": "application/json"
       },
